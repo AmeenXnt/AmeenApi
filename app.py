b3abf411cb262e616ae86158bf5b0b01ddade2eb
AmeenXnt <ameenserbots@gmail.com>
2024-08-22 03:41:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,4 @@ def download_instagram_video():
         return jsonify({"status": 500, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=8000)
-        
+    app.run(host='0.0.0.0', port=8000)
